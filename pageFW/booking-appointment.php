eca479df6d015e76b5e6a8e2d2af8b7e559bd4c1
STARRY99770 <angelaxueling0426@gmail.com>
2025-04-26 10:13:57 +0800

    Update booking-appointment.php

--- pageFW/booking-appointment.php
+++ pageFW/booking-appointment.php
@@ -2,10 +2,10 @@
 session_start();
 
 class Database {
-    private $host = "sql313.infinityfree.com";
-    private $user = "if0_38830936";
-    private $pass = "IxMNMzyIdNj";
-    private $dbname = "if0_38830936_ehealth";
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $dbname = "foreign_workers";
     public $conn;
 
     public function __construct() {
