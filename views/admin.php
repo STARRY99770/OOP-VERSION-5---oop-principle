30470d195a4aa1ac9119fcc7962bbdc87b14adb9
STARRY99770 <angelaxueling0426@gmail.com>
2025-04-26 10:29:28 +0800

    Update admin.php

--- views/admin.php
+++ views/admin.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-$servername = "sql313.infinityfree.com";
-$username = "if0_38830936";
-$password = "IxMNMzyIdNj"; 
-$database = "if0_38830936_ehealth";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "foreign_workers";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
